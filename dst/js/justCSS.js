511bb4ed53b6de577131f74c8720b572c7a5ea88
Jose Luis <fernandezmateojoseluis@gmail.com>
2017-12-17 19:41:59 +0100

    Final Files

---
+++ dst/js/justCSS.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  let isShown = false;
+  let menu = $('.mobile-navbar-menu');
+  let icon = $('.enable-nav').children('i');
+  $('.enable-nav').on('click', function(){
+    isShown = !isShown;
+    if(isShown){
+      menu.removeClass('animated fadeOutLeft');
+      menu.css('display', 'block');
+      menu.addClass('animated fadeInLeft');
+      icon.addClass('animated rotateIn');
+      icon.text('close');
+    }
+    if(!isShown){
+      menu.removeClass('animated fadeInLeft');
+      menu.addClass('animated fadeOutLeft');
+      icon.addClass('animated rotateIn');
+      icon.text('menu');
+    }
+  });
+  menu.on('animationend', function(){
+    if(!isShown){
+      menu.css('display', 'none');
+    }
+    icon.removeClass('animated rotateIn');
+  });
+  $('div.dropdown label').append('<i style="margin-right:2rem; line-height:12px;" class="material-icons right">keyboard_arrow_down</i>');
+  var num = 0;
+  $('div.dropdown').each(function(){
+    num++;
+    $(this).attr('id', 'dropdown-'+num);
+  });
+  $('div.dropdown').hover(function(){
+    $('#'+$(this).attr('id')).children('ul.dropdown-content').css('display', 'block');
+  },function(){
+    $('#'+$(this).attr('id')).children('ul.dropdown-content').css('display', 'none');
+  });
+});
